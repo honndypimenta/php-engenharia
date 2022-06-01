975fc5fed78122a0fcaf39818fc6981b3d1668c9
honndy pimenta <104370465+honndypimenta@users.noreply.github.com>
2022-06-01 01:03:12 -0300

    Create Train.php

---
+++ Train.php
@@ -0,0 +1,12 @@
+ public function train(Perceptron $p)
+ {
+     for ($i = -10000; $i < 10000; $i++) {
+         $x = rand(-100000, 100000);
+         $y = rand(-100000, 100000);
+         $answer = 1;
+         if ($y < $this->f($x)) {
+             $answer = -1;
+         }
+         $p->train($answer, $x, $y, 1);
+     }
+ }
